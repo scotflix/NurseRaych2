ef1623ee2ef1834169a266f35c1960d6d9967adf
scotflix <scotflix101@gmail.com>
2025-08-15 20:23:05 +0300

    1st commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,8 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// Change this to match your GitHub repo name
-const repoName = 'NurseRaych2';
-
-// GitHub Actions sets this env variable automatically
-const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
-
 export default defineConfig({
-  base: isGithubPages ? `/${repoName}/` : '/',
+  base: '/', // Always root for Vercel
   plugins: [react()],
   resolve: {
     alias: {
